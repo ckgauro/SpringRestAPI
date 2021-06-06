6c7a1441a406bf25ef3fa9afff142fcd275b996f
Chandra kishor Gauro <ckgauro@gmail.com>
2021-06-06 08:13:36 -0400

    Versioning our API

---
+++ RestWithSpringBootUdemy/06 RestWithSpringBootUdemy Versioning/src/test/java/com/gauro/VersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
