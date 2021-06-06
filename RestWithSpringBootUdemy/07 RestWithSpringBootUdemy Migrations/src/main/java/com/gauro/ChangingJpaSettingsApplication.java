f20c17febd706e5954fa1e3170f2a33aeaa97353
Chandra kishor Gauro <ckgauro@gmail.com>
2021-06-06 19:01:20 -0400

    Changing JPA settings

---
+++ RestWithSpringBootUdemy/07 RestWithSpringBootUdemy Migrations/src/main/java/com/gauro/ChangingJpaSettingsApplication.java
@@ -0,0 +1,13 @@
+package com.gauro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangingJpaSettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangingJpaSettingsApplication.class, args);
+	}
+
+}
