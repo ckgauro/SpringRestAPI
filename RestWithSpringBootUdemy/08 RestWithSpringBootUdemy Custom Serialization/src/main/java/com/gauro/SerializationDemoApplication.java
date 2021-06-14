260fde80a993b7d3e0ff8fa8b19ae94428b98e26
Chandra kishor Gauro <ckgauro@gmail.com>
2021-06-13 21:06:45 -0400

    Implementing custom JSON serialization

---
+++ RestWithSpringBootUdemy/08 RestWithSpringBootUdemy Custom Serialization/src/main/java/com/gauro/SerializationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gauro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerializationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerializationDemoApplication.class, args);
+	}
+
+}
