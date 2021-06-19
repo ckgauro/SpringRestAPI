10770f93b7a5a9d4a0d02322a0cb9184e6476976
Chandra kishor Gauro <ckgauro@gmail.com>
2021-06-19 17:23:29 -0400

    Implementing  Content negociation with YAML

---
+++ RestWithSpringBootUdemy/09 RestWithSpringBootUdemy Content Negociation/src/main/java/com/gauro/ContentnegociationApplication.java
@@ -0,0 +1,13 @@
+package com.gauro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentnegociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentnegociationApplication.class, args);
+	}
+
+}
