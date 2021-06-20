bac0348eda921cd3cb9e93ffd36c28fb6a362fac
Chandra kishor Gauro <ckgauro@gmail.com>
2021-06-20 11:25:21 -0400

    Implementing  Content negociation with YAML

--- RestWithSpringBootUdemy/09 RestWithSpringBootUdemy Content Negociation/src/main/java/com/gauro/serialization/converter/YamlJackson2HttpMessageConverter.java
+++ RestWithSpringBootUdemy/09 RestWithSpringBootUdemy Content Negociation/src/main/java/com/gauro/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -1,9 +1,15 @@
 package com.gauro.serialization.converter;
 
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
-
+//import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 /**
  * @author Chandra
  */
-//public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+//public final class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter{
+//
+//    public YamlJackson2HttpMessageConverter() {
+//        super(new YAMLMapper(), MediaType.parseMediaType("application/x-yaml"));
+//    }
+//
 //}
